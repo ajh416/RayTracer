3ae98e443859c02ec421ec0cb60f84475526df64
ajh416 <henryada@oregonstate.edu>
2022-12-07 12:56:52 -0800

    add premake for crossplatform build

---
+++ premake5.lua
@@ -0,0 +1,56 @@
+workspace "RayTracer"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "RayTracer"
+	location "RayTracer"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	systemversion "latest"
+	staticruntime "On"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/src/stb_image/**.cpp",
+		"%{prj.name}/src/stb_image/**.h",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/src/stb_image",
+	}
+
+	links 
+	{
+	}
+
+	defines
+	{
+	}
+
+	filter "configurations:Debug"
+		defines ""
+		symbols "On"
+
+	filter "configurations:Release"
+		defines ""
+		optimize "On"
